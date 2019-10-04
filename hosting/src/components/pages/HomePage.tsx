ee36407e8718188630c6d398e4bb8eef20e824fa
gorori <kousaku.maron@gmail.com>
2019-10-04 10:57:31 +0900

    feat: add link to home page

--- hosting/src/components/pages/HomePage.tsx
+++ hosting/src/components/pages/HomePage.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
 
 const HomePage = () => {
-  return <h1>ホーム</h1>
+  const classes = useStyles()
+
+  return (
+    <div className={classes.container}>
+      <h1>ホーム</h1>
+      <Link to="/terms">利用規約</Link>
+      <Link to="/webview-terms">利用規約(webview)</Link>
+      <Link to="/privacy">プライバシーポリシー</Link>
+      <Link to="/webview-privacy">プライバシーポリシー(webview)</Link>
+    </div>
+  )
 }
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexDirection: 'column'
+    }
+  })
+)
 
 export default HomePage
