889e9775031c9b1d8a0dfa82ba412ba5e0a3ca69
kosaku kurino <kousaku.maron@gmail.com>
2019-10-22 16:53:16 +0900

    feat: create lp home frame

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import CssBaseline from '@material-ui/core/CssBaseline'
 import { HomePage, TermsPage, PrivacyPage, TermsWebviewPage, PrivacyWebviewPage } from './components/pages'
 
 const App = () => (
   <Router>
     <div>
+      <CssBaseline />
       <Switch>
         <Route exact path={['/home', '/']} component={HomePage} />
         <Route path="/terms" component={TermsPage} />
--- hosting/src/components/pages/HomePage.tsx
+++ hosting/src/components/pages/HomePage.tsx
@@ -1,17 +1,43 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
 const HomePage = () => {
   const classes = useStyles()
 
   return (
     <div className={classes.container}>
-      <h1>ホーム</h1>
-      <Link to="/terms">利用規約</Link>
-      <Link to="/webview-terms">利用規約(webview)</Link>
-      <Link to="/privacy">プライバシーポリシー</Link>
-      <Link to="/webview-privacy">プライバシーポリシー(webview)</Link>
+      <div className={classes.companyWrapper}>
+        <Typography className={classes.companyText}>Gorori.llc</Typography>
+      </div>
+      <div className={classes.messageContainer}>
+        <Typography className={classes.subtitleText}>
+          飲み会 マッチングアプリ
+        </Typography>
+        <div className={classes.titleWrapper}>
+          <Typography className={classes.titleText}>
+            Nomoca
+          </Typography>
+        </div>
+        <div className={classes.actionArea}>
+          <div className={classes.appleWrapper}>
+            <Button variant='outlined' color='primary' fullWidth={true}>
+              <Typography className={classes.whiteText}>Apple Store</Typography>
+            </Button>
+          </div>
+          <div className={classes.googleWrapper}>
+            <Button variant='outlined' color='primary' fullWidth={true}>
+              <Typography className={classes.whiteText}>Google Store</Typography>
+            </Button>
+          </div>
+        </div>
+        
+        <Typography className={classes.whiteText}>
+          <Link className={classes.linkText} to="/terms">利用規約</Link>および<Link className={classes.linkText} to="/privacy">プライバシーポリシー</Link>に同意して、アプリをインストール。
+        </Typography> 
+      </div>
     </div>
   )
 }
@@ -19,8 +45,59 @@ const HomePage = () => {
 const useStyles = makeStyles(() =>
   createStyles({
     container: {
+      position: 'relative',
+      width: '100vw',
+      height: '100vh',
+      backgroundColor: 'black',
+      padding: 96
+    },
+    messageContainer: {
+      paddingTop: 100,
+      width: 278,
+    },
+    actionArea: {
       display: 'flex',
-      flexDirection: 'column'
+      width: '100%',
+      paddingBottom: 12
+    },
+    companyWrapper: {
+      position: 'absolute',
+      top: 46,
+      right: 46
+    },
+    titleWrapper: {
+      paddingBottom: 24
+    },
+    appleWrapper: {
+      flex: 1,
+      paddingRight: 6
+    },
+    googleWrapper: {
+      flex: 1,
+      paddingLeft: 6
+    },
+    companyText: {
+      fontSize: 18,
+      color: 'white'
+    },
+    titleText: {
+      fontSize: 56,
+      color: 'white',
+      letterSpacing: 12
+    },
+    subtitleText: {
+      fontSize: 24,
+      color: 'white',
+    },
+    linkText: {
+      fontSize: 12,
+      color: 'white',
+      textDecoration: 'none',
+      fontWeight: 'bold'
+    },
+    whiteText: {
+      fontSize: 12,
+      color: 'white',
     }
   })
 )
