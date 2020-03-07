f3abe4b201885e488802d4ff2f41b2b92a3a9117
kosaku kurino <kousaku.maron@gmail.com>
2020-03-07 18:17:18 +0900

    fix: available firebase cli lib

--- scripts/deploy_functions.js
+++ scripts/deploy_functions.js
@@ -10,7 +10,7 @@ const functions = require('../functions/lib')
 
 _.chunk(Object.keys(functions), 15).forEach(names => {
   const only = names.map(name => `functions:${name}`).join()
-  const command = `firebase deploy --force --only ${only} --project ${project} --token ${token}`
+  const command = `yarn firebase deploy --force --only ${only} --project ${project} --token ${token}`
   console.log(`target functions: ${only}`)
   execSync(command, { stdio: 'inherit' })
 })
