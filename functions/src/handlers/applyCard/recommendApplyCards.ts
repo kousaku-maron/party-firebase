4fcb7ac802342020f25035d9778056e38fa9aa31
tabikaeru <moriokayuixxx@gmail.com>
2020-03-25 14:34:22 +0900

    feat:create card by Tags

--- functions/src/handlers/applyCard/recommendApplyCards.ts
+++ functions/src/handlers/applyCard/recommendApplyCards.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions'
 import { firestore } from 'firebase-admin'
-import { createDocument, CreateApplyCard, buildParty, Group, buildGroup, recommendPartyType } from '../../entities'
+import { createDocument, CreateApplyCard, buildParty, Group, buildGroup, recommendApplyCardTags } from '../../entities'
 import { shuffle } from '../../services/util'
 
 const recommendCardNumber = 3
@@ -10,65 +10,70 @@ export const recommendApplyCards = functions.https.onCall(async () => {
   const db = firestore()
   let batch = db.batch()
   const partiesRef = db.collection('parties')
-  const tagedPartyRef = partiesRef
-    .where('enabled', '==', true)
-    .where('tags', 'array-contains-any', [recommendPartyType])
-  const partySnapShot = await tagedPartyRef.get()
 
-  if (partySnapShot.docs.length != 1) return
+  const partyTagTasks = recommendApplyCardTags.map(async recommendApplyCardTag => {
+    const tagedPartyRef = partiesRef
+      .where('enabled', '==', true)
+      .where('tags', 'array-contains-any', [recommendApplyCardTag])
+    const partySnapShot = await tagedPartyRef.get()
 
-  const party = buildParty(partySnapShot.docs[0].id!, partySnapShot.docs[0].data()!)
-  const partyID = party.id
+    if (partySnapShot.docs.length != 1) return
 
-  const groupsRef = partiesRef.doc(partyID).collection('groups')
-  const groupsSnapShot = await groupsRef.get()
-  const groups = groupsSnapShot.docs.map((doc: firestore.DocumentData) => {
-    return buildGroup(doc.id!, doc.data()!)
-  })
+    const party = buildParty(partySnapShot.docs[0].id!, partySnapShot.docs[0].data()!)
+    const partyID = party.id
 
-  const tasks = groups.map(async (group, index) => {
-    const { id: MyGroupID, organizer } = group
-
-    const shuffledGroups: Group[] = shuffle<Group>(groups.filter(group => group.id !== MyGroupID))
-    const recommendedGroups: Group[] = shuffledGroups.slice(0, recommendCardNumber)
-
-    const applyCardsRef = db
-      .collection('users')
-      .doc(organizer.uid)
-      .collection('appliedCards')
-    const oldCardsRef = applyCardsRef.where('type', '==', recommendPartyType)
-
-    const oldCardsRefSnapShot = await oldCardsRef.get()
-
-    oldCardsRefSnapShot.docs.map(doc => {
-      const oldCardRef = doc.ref
-      batch.delete(oldCardRef)
+    const groupsRef = partiesRef.doc(partyID).collection('groups')
+    const groupsSnapShot = await groupsRef.get()
+    const groups = groupsSnapShot.docs.map((doc: firestore.DocumentData) => {
+      return buildGroup(doc.id!, doc.data()!)
     })
 
-    const createCardTasks = recommendedGroups.map(async (recommendedGroup, groupIndex) => {
-      if (((groupIndex + 1 + recommendCardNumber) * (index + 1)) % maximumBatchSize == 0) {
-        await batch.commit()
-        batch = db.batch()
-      }
-      //MEMO: 将来的に同伴ユーザーも入れることを考えて，membersは配列にしている
-      batch.set(
-        applyCardsRef.doc(),
-        createDocument<CreateApplyCard>({
-          partyID: partyID,
-          groupID: recommendedGroup.id,
-          organizerUID: recommendedGroup.organizer.uid,
-          party: party,
-          type: recommendPartyType,
-          members: [recommendedGroup.organizer]
-        }),
-        { merge: true }
-      )
+    const userTasks = groups.map(async (group, index) => {
+      const { id: MyGroupID, organizer } = group
+
+      const shuffledGroups: Group[] = shuffle<Group>(groups.filter(group => group.id !== MyGroupID))
+      const recommendedGroups: Group[] = shuffledGroups.slice(0, recommendCardNumber)
+
+      const applyCardsRef = db
+        .collection('users')
+        .doc(organizer.uid)
+        .collection('appliedCards')
+      const oldCardsRef = applyCardsRef.where('type', '==', party.type)
+
+      const oldCardsRefSnapShot = await oldCardsRef.get()
+
+      oldCardsRefSnapShot.docs.map(doc => {
+        const oldCardRef = doc.ref
+        batch.delete(oldCardRef)
+      })
+
+      const createCardTasks = recommendedGroups.map(async (recommendedGroup, groupIndex) => {
+        if (((groupIndex + 1 + recommendCardNumber) * (index + 1)) % maximumBatchSize == 0) {
+          await batch.commit()
+          batch = db.batch()
+        }
+        //MEMO: 将来的に同伴ユーザーも入れることを考えて，membersは配列にしている
+        batch.set(
+          applyCardsRef.doc(),
+          createDocument<CreateApplyCard>({
+            partyID: partyID,
+            groupID: recommendedGroup.id,
+            organizerUID: recommendedGroup.organizer.uid,
+            party: party,
+            type: party.type,
+            members: [recommendedGroup.organizer]
+          }),
+          { merge: true }
+        )
+      })
+
+      await Promise.all(createCardTasks)
     })
 
-    await Promise.all(createCardTasks)
+    await Promise.all(userTasks)
   })
 
-  await Promise.all(tasks)
+  await Promise.all(partyTagTasks)
 
   await batch.commit()
 
